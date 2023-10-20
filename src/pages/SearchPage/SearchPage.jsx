078234bcb03ce1d2b973440473909576083d7dbb
Shea Raymond <shea.raymond01@gmail.com>
2023-10-20 17:05:40 -0500

    Adding functionality to searchbar.

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import AuthContext from "../../context/AuthContext";
 import useCustomForm from "../../hooks/useCustomForm";
 import { Link } from "react-router-dom";
@@ -7,29 +7,33 @@ import SearchBar from "../../components/SearchBar/SearchBar";
 import axios from "axios";
 
 const SearchPage = () => {
-	const [searchTerm, setSearchTerm] = useState("");
+	const [searchInput, setSearchInput] = useState("");
 	const [searchResults, setSearchResults] = useState([]);
 
-    const fetchBooks = async () => {
-        try {
-            let search = searchTerm.toLowerCase();
-            let response = await axios.get()  //enter url in the parenthesis
-            setSearchResults(response.data)
-        } catch (error) {
-            console.log("Error in fetchBooks: ", error);
-            
-        }
-    
-    );
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        fetchBooks();
-    } 
+	const fetchBooks = async () => {
+		try {
+			let searchTerm = searchInput.toLowerCase();
+			let response = await axios.get(
+				`https://www.googleapis.com/books/v1/volumes/${searchTerm}/`
+			);
+			console.log(response.data);
+		} catch (error) {
+			console.log("Error in fetchBooks: ", error);
+		}
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		fetchBooks();
+	};
 
 	return (
 		<div className='container'>
 			<h1>Search Library</h1>
-			<SearchBar />;
+			<SearchBar
+				searchInput={searchInput}
+				setSearchInput={setSearchInput}
+				handleSubmit={handleSubmit}
+			/>
 		</div>
 	);
 };
